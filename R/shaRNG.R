1543be6a2d4da54fc7d8bb0de70ff5f70ba5b257
Luchang Jin <waterret@gmail.com>
2016-10-02 21:11:27 -0400

    add few utility functions

--- R/shaRNG.R
+++ R/shaRNG.R
@@ -1,9 +1,36 @@
+#' Splittable pseudo random number generator
+#'
+#'
+#' @description Provides a random number generator based on sha2
+#' which enables easy generation of independent and parallel
+#' pseudo random number substreams
+#'
+#' @docType package
+#' @name shaRNG_package
+NULL
+
+
+#' Get current state of the RNG
+#'
+#' @return current state, which is
+#' the integer vector of the current value of \code{.Random.seed}. For
+#' more details, see \code{\link{.Random.seed}}.
+#'
 RNG.get.state <- function() {
 	return(.Random.seed)
 }
 
 RNG.set.state <- function(state) {
 	.Random.seed <<- state
+}
+
+RNG.sync <- function(cl = NULL) {
+	sync <- function(state) {
+		library(shaRNG)
+		RNG.set.state(state)
+	}
+	clusterCall(cl, sync, RNG.get.state())
+	invisible(NULL)
 }
 
 RNG.root.state <- function() {
@@ -30,6 +57,10 @@ RNG.set.split.state <- function(state, sindex) {
 	RNG.set.state(RNG.split.state(state, sindex))
 }
 
+RNG.jump <- function(sindex = "") {
+	RNG.set.split.state(RNG.get.state(), sindex)
+}
+
 RNG.save.eval <- function(expr) {
 	old.state <- RNG.get.state()
 	force(expr)
@@ -45,3 +76,27 @@ RNG.split.eval <- function(sindex, expr) {
 }
 
 `%RNG.split.eval%` <- RNG.split.eval
+
+#' @param root.state the start state for splitting,
+#' which is an interger vector in
+#' the form of .Random.seed. If state is NULL, then the current state of the
+#' generator will be automatically used.
+#' @param indices a vector of unique strings/integers for the indices of
+#' each splitted stream.
+#' @param n.stream desired number of splitted streams. A non-negative number,
+#' which will be rounded down if fractional
+#'
+GetSplitStates <- function(n.stream = NULL,
+						   indices = seq(n.stream),
+						   root.state = NULL) {
+
+	if (is.null(root.state))
+		root.state <- RNG.get.state()
+	sub.states <- lapply(indices, function(sindex)
+						 RNG.split.state(root.state,
+										 sindex))
+	names(sub.states) <- indices
+	return(list(sub.states = sub.states, root.state = root.state))
+}
+
+
